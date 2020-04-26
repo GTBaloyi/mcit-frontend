6198912a2303c251df45c8b90a0d6a19deececf7
aluwaninelufule <alu.nelufule@gmail.com>
2020-04-26 03:22:52 +0200

    registration screen design

--- src/app/components/client-registration/client-registration.component.ts
+++ src/app/components/client-registration/client-registration.component.ts
@@ -1,44 +1,79 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/User';
-import { ClientReg } from 'src/app/models/client-reg.model';
-import { NgForm } from '@angular/forms';
+import {Component, OnChanges, OnInit} from '@angular/core';
+import {Subject} from "rxjs";
+import {AuthenticationService} from "../../services/AuthenticationService";
+import {AuthGuard} from "../../services/auth.guard";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
+import {Register, User} from "../../models/User";
 
 
 @Component({
-  selector: 'client-registration',
-  templateUrl: './client-registration.component.html',
-  styleUrls: ['./client-registration.component.scss']
+    selector: 'client-registration',
+    templateUrl: './client-registration.component.html',
+    styleUrls: ['./client-registration.component.scss']
 })
-export class ClientRegistrationComponent implements OnInit{
+export class ClientRegistrationComponent implements OnChanges, OnInit  {
 
- 
-user: ClientReg
+    private title: string;
+    private lastName: string;
+    private companyName: string;
+    private vatNumber: string;
+    private phone: string;
+    private email: string;
+    private website: string;
+    private addressLine1: string;
+    private addressLine2: string;
+    private city: string;
+    private province: string;
+    private postalCode: string;
+    private country: string;
+    isLoading = new Subject<boolean>();
+    private result: Register;
 
-ngOnInit(){
-  this.resetForm();
+    ngOnInit() {
 
- }
+    }
 
- resetForm (form?:NgForm)
- {
-   if(form !=null)
-form.reset();
-this.user = {
-  Username:'',
-  email:'',
-  NameOfCompany:'',
-  VatNumberIdentification:'',
-  PhoneNumber:'',
-  Password:'',
-  ConfirmPasword:'',
-  AddressLine1:'',
-  AdrdressLine2:'',
-  Province:'',
-  City:''
 
-  }
+    ngOnChanges() {
+    }
 
- }
+
+    constructor(public authenticationService: AuthenticationService, public authGuard: AuthGuard, private _snackBar: MatSnackBar, private router: Router) {
+
+    }
+
+
+    openSnackBar(message: string, action: string) {
+        this._snackBar.open(message, action, {
+            duration: 10000,
+        });
+    }
+
+    singUp() {
+
+        let newUser: Register = null;
+
+        this.isLoading.next(true);
+
+        this.authenticationService.register(newUser).subscribe(
+            (data: Register) => {
+                this.result = data;
+
+            },
+            error => {
+                console.log(error);
+                this.openSnackBar('Registration failed, please try again later', 'Ok');
+                this.isLoading.next(false);
+            },
+            () => {
+                this.openSnackBar('Registration successful', 'Ok');
+                this.isLoading.next(false);
+                this.router.navigateByUrl('/landing-page');
+
+            }
+        )
+    }
 
 }
 
